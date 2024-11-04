d9b1631a6c2f87f5db7312f3ac2bdd0e4701ef8f
nh40 <imbovoebozestvo@gmail.com>
2024-11-04 16:42:05 +0700

    start

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
+import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
+import * as cookieParser from 'cookie-parser'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
+
+	const config = app.get(ConfigService)
+
+	app.use(cookieParser(config.getOrThrow('COOKIES_SECRET')))
+
+	app.setGlobalPrefix('api')
 	await app.listen(4200)
 }
 bootstrap()
