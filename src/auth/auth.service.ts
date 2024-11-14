f50fc431accffab6fb035261a2a430a46cc38367
nh40 <imbovoebozestvo@gmail.com>
2024-11-15 04:04:44 +0700

    login and logout

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,15 +3,23 @@ import {
 	ConflictException,
 	Injectable,
 	InternalServerErrorException,
+	NotFoundException,
+	UnauthorizedException,
 } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
 import { AuthMethod } from '@prisma/__generated__'
 import { User } from '@prisma/__generated__/edge'
-import { Request } from 'express'
+import { verify } from 'argon2'
+import { Request, Response } from 'express'
+import { LoginDto } from './dto/login.dto'
 import { RegisterDto } from './dto/register.dto'
 
 @Injectable()
 export class AuthService {
-	public constructor(private readonly userService: UserService) {}
+	public constructor(
+		private readonly userService: UserService,
+		private readonly configService: ConfigService
+	) {}
 
 	public async register(req: Request, dto: RegisterDto) {
 		const isExist = await this.userService.findByEmail(dto.email)
@@ -34,9 +42,41 @@ export class AuthService {
 		return this.saveSession(req, newUser)
 	}
 
-	public async login() {}
+	public async login(req: Request, dto: LoginDto) {
+		const user = await this.userService.findByEmail(dto.email)
 
-	public async logout() {}
+		if (!user || !user.password) {
+			throw new NotFoundException(
+				'Пользователь не найден. Пожалуйста, проверьте введенные данные'
+			)
+		}
+
+		const isValidPassword = await verify(user.password, dto.password)
+
+		if (!isValidPassword) {
+			throw new UnauthorizedException(
+				'Неверный пароль. Пожалуйста, попробуйте еще раз или восстановите пароль, если забыли его.'
+			)
+		}
+
+		return this.saveSession(req, user)
+	}
+
+	public async logout(req: Request, res: Response): Promise<void> {
+		return new Promise((resolve, reject) => {
+			req.session.destroy(err => {
+				if (err) {
+					return reject(
+						new InternalServerErrorException(
+							'Не удалось завершить сессию. Возможно, возникла проблема с сервером или сессия уже была завершена.'
+						)
+					)
+				}
+				res.clearCookie(this.configService.getOrThrow<string>('SESSION_NAME'))
+				resolve()
+			})
+		})
+	}
 
 	private async saveSession(req: Request, user: User) {
 		return new Promise((resolver, reject) => {
